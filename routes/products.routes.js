6a7486e41b453a8863863d15ed186198f48b35fd
Adarsh Agrahari <agrahariadarsh68@gmail.com>
2025-03-31 21:22:20 +0530

    com

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const Product = require('../model/product.model');
+const { route } = require('./user.routes');
 
 //get all products
 router.get('/products', async(req, res) =>{
@@ -83,3 +84,22 @@ router.get('/category/:category', async (req, res)=>{
 
 
 //get products by seller id
+router.get('/products/seller/:sellerId', async (req, res)=>{
+    try{
+
+        const sellerId = req.params.sellerId;
+
+        const products = await Product.find({seller: sellerId});
+
+        if(products.length === 0){
+
+            return res.status(404).json({message: "No product found."});
+        }
+
+        res.status(200).json({message: "product found", products:product});
+    }catch(error){
+
+        res.status(500).json({message: "error fetching products", error: error.message});
+    }
+});
+module.exports = router;
