69a7fc63d29a31e206d8f9e3993dbcca8e93aaac
Adarsh Agrahari <agrahariadarsh68@gmail.com>
2025-04-01 16:26:13 +0530

    com

--- index.js
+++ index.js
@@ -1,27 +1,24 @@
 const express = require('express');
-const { db } = require('./model/user.modal');
 const app = express();
 const mongoose = require('mongoose');
-require('dotenv').config();
 const userRoutes = require('./routes/user.routes');
+const productRoutes  = require('./routes/products.routes');
+require('dotenv').config();
 
 mongoose.connect(process.env.MONGO_URI)
-.then(()=>{
+.then(() => {
   console.log('Connected to MongoDB');
 })
 .catch(err => console.log(err));
 
 app.use(express.json());
 
-app.use('/user', userRoutes);
-
+app.use('/product', productRoutes);
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
-});
-
+})
 
 app.listen(process.env.PORT, () => {
-  console.log('Server is running on port 3000');
-});
-
+  console.log(`Server is running on port ${process.env.PORT}`);
+})
