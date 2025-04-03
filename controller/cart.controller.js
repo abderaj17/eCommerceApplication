46c4b9a031086c92e2a307244cbe650927a0c65a
Adarsh Agrahari <agrahariadarsh68@gmail.com>
2025-04-03 15:58:18 +0530

    com

--- controller/cart.controller.js
+++ controller/cart.controller.js
@@ -23,3 +23,78 @@ exports.getCart = async (req, res)=>{
  }
 };
 
+exports.addToCart = async (req, res) => {
+    try {
+        const {productId, quantity = 1} = req.body;
+
+        const product = await Product.findById(productId);
+        if(!product){
+            return res.status(404).json({
+                success: false,
+                message: 'Product not found'
+            });
+        }
+
+        let cart = await Cart.findOne({ user: req.user.id});
+
+        if(!cart){
+            cart = new Cart({user: req.user.id, items:[]});
+        }
+
+        const existingItem = cart.items.find(
+            item => item.product.toString() === productId
+        );
+
+        if(existingItem){
+            existingItem.quantity += quantity;
+        }else{
+            cart.items.push({ product:productId, quantity});
+        }
+
+        await cart.save();
+
+        res.status(200).json({
+            success: true,
+            cart
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: 'Error adding to cart',
+            error: error.message
+        })
+    }
+};
+
+exports.removeFromCart = async (req, res) =>{
+    try{
+        const { itemId } = req.params;
+
+        const cart = await Cart.findOneAndUpdate(
+            {user: req.user.id},
+            {$pull: {items:{_id:itemId}}},
+            {new: true}
+        ).populate('items.product');
+
+        if(!cart){
+            return res.status(404).json({
+                success: false,
+                message: 'Cart not found'
+            });
+        }
+        res.status(200).json({
+            success: true,
+            cart
+        });
+    }catch(error){
+        res.status(500).json({
+            success: false,
+            message: 'Error removing item form cart',
+            error:error.message
+        })
+    }
+};
+
+exports.clearCart = async (req, res) => {
+    
+}
